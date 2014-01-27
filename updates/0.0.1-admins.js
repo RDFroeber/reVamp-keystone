86d525b37f0e9894a48cd94609ad583598056b3e
Freelancer <freelancer@Infor-sedna-10.local>
2014-01-27 16:46:56 -0500

    Application name changed in web.js andd admin email and password added to admin.js

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -3,14 +3,13 @@ var keystone = require('keystone'),
 	User = keystone.list('User');
 
 var admins = [
-	{ email: 'demo@keystonejs.com', password: 'demo', name: { first: 'Demo', last: 'User' } }
+	{ email: 'demo@reVamp.com', password: 'demo', name: { first: 'Jane', last: 'Doe' } }
 ];
 
 function createAdmin(admin, done) {
 	User.model.findOne({ email: admin.email }).exec(function(err, user) {
 		var newAdmin = new User.model(admin);
 		newAdmin.isAdmin = true;
-		newAdmin.isProtected = true;
 		newAdmin.save(function(err) {
 			if (err) {
 				console.error("Error adding admin " + admin.email + " to the database:");
